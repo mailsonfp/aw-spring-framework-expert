95fd6fdf92c95d23039be2248187ff49b228c1cf
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-12-31 23:12:09 -0300

    Criando repositório - Aula 3

---
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cerveja {
+	
+	private String sku;
+	private String nome;
+	
+}
