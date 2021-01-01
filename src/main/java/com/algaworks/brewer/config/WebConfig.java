d5f4fa71cf7999151166c3b31ebbc30f47ce2232
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2021-01-01 19:58:51 -0300

    Aula 4

---
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableCaching
+@EnableAsync
+public class WebConfig implements WebMvcConfigurer {
+
+}
--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,5 +1,8 @@
 package com.algaworks.brewer.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +10,13 @@ import lombok.Setter;
 @Setter
 public class Cerveja {
 	
+	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
+	
+	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
+	
+	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
+	private String descricao;	
 	
 }
