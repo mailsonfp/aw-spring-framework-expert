85bc4f1fc88c0fa1dca8ba6046bedc6f8666b698
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2021-01-02 12:17:05 -0300

    Aula 5.7 a 5.10

---
+++ src/main/java/com/algaworks/brewer/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.algaworks.brewer.model.Cerveja;
+
+@Controller
+public class ClienteController {
+	
+	@RequestMapping("/clientes/novo")
+	public String novo(Cerveja cerveja) {
+		return "cliente/CadastroCliente";
+	}
+}
