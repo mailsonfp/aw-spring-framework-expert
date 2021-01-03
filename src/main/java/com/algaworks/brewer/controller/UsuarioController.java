363f726a57cd6b64b03ae4107d6c60964a57db56
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2021-01-02 22:11:06 -0300

    Aula 5.11 a 5.15

---
+++ src/main/java/com/algaworks/brewer/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsuarioController {
+	
+	@RequestMapping(path = "/usuarios/novo")
+	public String novo() {
+		return "/usuario/CadastroUsuario";
+	}
+}
