9206994970c5ed6165845b099ea9442f00a7e4a1
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2021-01-04 23:28:16 -0300

    Aula 10

--- src/main/java/com/algaworks/brewer/controller/EstiloController.java
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -1,13 +1,39 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.service.EstiloService;
 
 @Controller
 public class EstiloController {
 	
+	@Autowired
+	EstiloService estiloService;
+	
 	@RequestMapping(path = "estilos/novo")
-	public String novo() {
-		return "estilo/CadastroEstilo";
+	public ModelAndView novo(Estilo estilo) {
+		return new ModelAndView("estilo/CadastroEstilo");
+	}
+
+	@RequestMapping(path = "estilos/novo", method = RequestMethod.POST)
+	public ModelAndView cadastrar(@Valid Estilo estilo, BindingResult result, Model model, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			return novo(estilo);
+		}
+		
+		estiloService.salvar(estilo);
+		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso!");
+		return new ModelAndView("redirect:/estilos/novo");
 	}
 }
--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -11,11 +11,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.algaworks.brewer.model.enums.Origem;
 import com.algaworks.brewer.model.enums.Sabor;
+import com.algaworks.brewer.validation.SKU;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -31,34 +36,49 @@ public class Cerveja {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
-
+	
+	@SKU
 	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
 
 	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
 
-	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
+	@NotBlank(message = "A descrição é obrigatória")
+	@Size(max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
 	private String descricao;
 
+	@NotNull(message = "Valor é obrigatório")
+	@DecimalMin(value = "0.50", message = "O valor da cerveja deve ser maior que R$0,50")
+	@DecimalMax(value = "9999999.99", message = "O valor da cerveja deve ser menor que R$9.999.999,99")
 	private BigDecimal valor;
 
+	@NotNull(message = "O teor alcóolico é obrigatório")
+	@DecimalMax(value = "100.0", message = "O valor do teor alcóolico deve ser menor que 100")
 	@Column(name = "teor_alcoolico")
 	private BigDecimal teorAlcoolico;
-
+	
+	@NotNull(message = "A comissão é obrigatória")
+	@DecimalMax(value = "100.0", message = "A comissão deve ser igual ou menor que 100")
 	private BigDecimal comissao;
-
+	
+	@NotNull(message = "A quantidade em estoque é obrigatória")
+	@Max(value = 9999, message = "A quantidade em estoque deve ser menor que 9.999")
 	@Column(name = "quantidade_estoque")
 	private Integer quantidadeEstoque;
 
+	@NotNull(message = "A origem é obrigatória")
 	@Enumerated(EnumType.STRING)
 	private Origem origem;
 
+	@NotNull(message = "O sabor é obrigatório")
 	@Enumerated(EnumType.STRING)
 	private Sabor sabor;
 
+	@NotNull(message = "O estilo é obrigatório")
 	@ManyToOne
 	@JoinColumn(name = "codigo_estilo")
-	private Estilo estilo;	
+	private Estilo estilo;
+	
 	
 }
